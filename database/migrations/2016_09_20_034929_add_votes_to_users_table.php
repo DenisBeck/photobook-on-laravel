47b7d023f843e37899954dc4e2d8893d12c70495
DenisBeck <beckass86@gmail.com>
2016-09-20 09:12:01 +0500

    Подготовка проекта, создание базы данных

---
+++ database/migrations/2016_09_20_034929_add_votes_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddVotesToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('lastname')->after('name');
+            $table->string('avatar')->after('email');
+            $table->text('about')->after('avatar');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropItem('lastname');
+            $table->dropItem('avatar');
+            $table->dropItem('about');
+        });
+    }
+}
